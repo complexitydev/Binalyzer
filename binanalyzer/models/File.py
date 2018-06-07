6c70b594b68761bb548c4c635024f012899f7239
complexdev <bennyh6813@gmail.com>
2018-06-07 01:43:43 -0400

    pep 8 formatting

--- binanalyzer/models/File.py
+++ binanalyzer/models/File.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import boto3
 import json
 
--- runsesrver.py
+++ runsesrver.py
@@ -1,6 +1,6 @@
 import os
 from binanalyzer import app
-#
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 80))
     app.run('127.0.0.1', port=port)
